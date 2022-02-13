9e52551ac64c488f4b474da0e678ee5fdff1f93a
Flikki1 <99621746+Flikki1@users.noreply.github.com>
2022-02-14 09:52:02 +1000

    Add files via upload

---
+++ rem-cli/rem-cli/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace rem_cli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.btnShare = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnCon
+            // 
+            this.btnCon.Location = new System.Drawing.Point(42, 302);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(136, 57);
+            this.btnCon.TabIndex = 0;
+            this.btnCon.Text = "Connect";
+            this.btnCon.UseVisualStyleBackColor = true;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            // 
+            // btnShare
+            // 
+            this.btnShare.Location = new System.Drawing.Point(318, 302);
+            this.btnShare.Name = "btnShare";
+            this.btnShare.Size = new System.Drawing.Size(136, 57);
+            this.btnShare.TabIndex = 1;
+            this.btnShare.Text = "ShareMyScreen";
+            this.btnShare.UseVisualStyleBackColor = true;
+            this.btnShare.Click += new System.EventHandler(this.btnShare_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(42, 220);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(412, 20);
+            this.txtPort.TabIndex = 2;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(42, 133);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(412, 20);
+            this.txtIP.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PORT";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.btnShare);
+            this.Controls.Add(this.btnCon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.Button btnShare;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ rem-cli/rem-cli/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Drawing.Imaging;
+using System.Runtime.Serialization.Formatters.Binary;
+using Gma.System.MouseKeyHook;
+using System.Runtime.InteropServices;
+
+using System.IO;
+
+namespace rem_cli
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool trackCursorPos(int x, int y);
+        private IKeyboardMouseEvents m_Events;
+        private readonly TcpClient tcpCli = new TcpClient();
+        private NetworkStream netStream;
+        private Point cliMousePoint = new Point();    
+        private int PortNum;
+        
+
+
+        private static Image CatchDesktop()
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshots = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(screenshots);
+            graphics.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+            return screenshots;
+        }
+    
+        private void SendDudes()
+        {
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            netStream = tcpCli.GetStream();
+            bFormatter.Serialize(netStream, CatchDesktop());
+        }
+
+        private void SendMousePos()
+        {
+
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            netStream = tcpCli.GetStream();
+            bFormatter.Serialize(netStream, cliMousePoint);
+            
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnShare.Enabled = false;
+            timer1.Interval = 120;
+            Unsubscribe();
+            Subscribe(Hook.GlobalEvents());
+        }
+
+        private void btnCon_Click(object sender, EventArgs e)
+        {
+            PortNum = int.Parse(txtPort.Text);
+            try
+            {
+                tcpCli.Connect(txtIP.Text, PortNum);
+                btnCon.Text = "Connncted";
+                MessageBox.Show("Работает заебал");
+                btnCon.Enabled = false;
+                btnShare.Enabled = true;
+
+            }
+
+            catch(Exception)
+            {
+                MessageBox.Show("Не работает заебал");
+                btnCon.Text = "Пососешь,ок?";
+            }
+
+        }
+
+        private void btnShare_Click(object sender, EventArgs e)
+        {
+            if (btnShare.Text.StartsWith("Share")) 
+            {              
+                timer1.Start();
+                btnShare.Text = "Останови это дерьмо";
+            }
+            else
+            {
+                timer1.Stop();
+                btnShare.Text = "ShareMyScreen";
+            }
+        }
+
+        private void Subscribe (IKeyboardMouseEvents events)
+        {
+            m_Events = events;
+            m_Events.MouseMove += M_Events_MouseMove;
+        }
+
+        private void M_Events_MouseMove(object sender, MouseEventArgs e)
+        {
+            cliMousePoint = e.Location;
+        }
+
+        private void Unsubscribe()
+        {
+            if (m_Events == null) return;
+            m_Events.MouseMove -= M_Events_MouseMove;
+            m_Events.Dispose();
+            m_Events = null;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendDudes();
+            SendMousePos();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Unsubscribe();
+        }
+    }
+}
