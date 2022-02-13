77e9a6377a3336d00adb749b824dcff9fc3d881b
Flikki1 <99621746+Flikki1@users.noreply.github.com>
2022-02-14 09:53:57 +1000

    Add files via upload

---
+++ rem-host/rem-host/Form2.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace rem_host
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cliM_pointX = new System.Windows.Forms.Label();
+            this.cliM_pointY = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cliM_pointX
+            // 
+            this.cliM_pointX.AutoSize = true;
+            this.cliM_pointX.Location = new System.Drawing.Point(728, 13);
+            this.cliM_pointX.Name = "cliM_pointX";
+            this.cliM_pointX.Size = new System.Drawing.Size(35, 13);
+            this.cliM_pointX.TabIndex = 1;
+            this.cliM_pointX.Text = "label1";
+            // 
+            // cliM_pointY
+            // 
+            this.cliM_pointY.AutoSize = true;
+            this.cliM_pointY.Location = new System.Drawing.Point(728, 36);
+            this.cliM_pointY.Name = "cliM_pointY";
+            this.cliM_pointY.Size = new System.Drawing.Size(35, 13);
+            this.cliM_pointY.TabIndex = 2;
+            this.cliM_pointY.Text = "label2";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cliM_pointY);
+            this.Controls.Add(this.cliM_pointX);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label cliM_pointX;
+        private System.Windows.Forms.Label cliM_pointY;
+    }
+}
---
+++ rem-host/rem-host/Form2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace rem_host
+{
+    public partial class Form2 : Form
+    {
+        private readonly int port;
+
+        public Form2(TcpClient tcpClient)
+        {
+            this.tcpClient = tcpClient;
+        }
+
+        private TcpClient tcpClient;
+        private TcpListener tcpListener;
+        private NetworkStream netSteram;
+        private NetworkStream netSteram2;
+
+
+        private readonly Thread Listening;
+        private readonly Thread GetCapture;
+        private readonly Thread GetCliMousePonit;
+        private Point cliCurrentMousePoint;
+
+        public Thread GetCliMousePonit1 => GetCliMousePonit;
+
+        public Form2(int Port)
+        {
+            port = Port;
+            tcpClient = new TcpClient();
+            Listening = new Thread(RodinaSlishit);
+            GetCapture = new Thread(RecieveDudes);
+            GetCliMousePonit = new Thread(RecieveCliM_point);
+
+
+
+            InitializeComponent();
+        }
+
+        private void RecieveCliM_point()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            while (tcpClient.Connected)
+            {
+                netSteram2 = tcpClient.GetStream();
+                cliM_pointX.Invoke((Action)delegate
+                {
+                    cliCurrentMousePoint = (Point)binFormat.Deserialize(netSteram2);
+
+                    cliM_pointX.Text = "x:" + cliCurrentMousePoint.X.ToString() + "y:" + cliCurrentMousePoint.Y.ToString();
+                });
+
+            }
+        }
+
+        private void RodinaSlishit()
+        {
+            while (!tcpClient.Connected)
+            {
+                tcpListener.Start();
+                tcpClient = tcpListener.AcceptTcpClient();
+            }
+            GetCapture.Start();
+            GetCliMousePonit.Start();
+        }
+
+        private void RodinaNeSlishit()
+        {
+            tcpListener.Stop();
+            tcpClient = null;
+            if (Listening.IsAlive)
+            {
+                Listening.Abort();
+            }
+            if (GetCapture.IsAlive)
+            {
+                GetCapture.Abort();
+            }
+            if (GetCliMousePonit.IsAlive)
+            {
+                GetCliMousePonit.Abort();
+            }
+        }
+
+        private void RecieveDudes()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            while (tcpClient.Connected)
+            {
+                netSteram = tcpClient.GetStream();
+                pictureBox1.Invoke((Action)delegate
+                {
+                    pictureBox1.Image = (Image)binFormat.Deserialize(netSteram);
+                });
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            tcpListener = new TcpListener(IPAddress.Any, port);
+            Listening.Start();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            RodinaNeSlishit();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
